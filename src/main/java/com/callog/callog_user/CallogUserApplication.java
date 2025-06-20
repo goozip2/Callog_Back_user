70a2e15a8d3d6d8461b4d0c88e07c6c192447281
Imdahee <ldhdh1832@gmail.com>
2025-06-20 16:26:29 +0900

    Initial commit

---
+++ src/main/java/com/callog/callog_user/CallogUserApplication.java
@@ -0,0 +1,13 @@
+package com.callog.callog_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallogUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallogUserApplication.class, args);
+	}
+
+}
