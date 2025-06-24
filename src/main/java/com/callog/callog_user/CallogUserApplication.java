a810c7d39c96ac8895f8dd859436b01bffc6e29d
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-06-25 05:17:50 +0900

    feat: User 서비스와의 remote 통신 openfeign remote 서비스 구현

--- src/main/java/com/callog/callog_user/CallogUserApplication.java
+++ src/main/java/com/callog/callog_user/CallogUserApplication.java
@@ -3,8 +3,10 @@ package com.callog.callog_user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+@EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class CallogUserApplication {
